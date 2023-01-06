8edfe109bfdec14ef897e2dde80d0bcf66ae0459
PAVANI <PAVANI@DESKTOP-CJH8IVK>
2023-01-06 14:43:41 +0530

    my first commit

---
+++ devaharsha/src/main/java/DEFALUTPACKAGE/COM/Tc1_test.java
@@ -0,0 +1,15 @@
+package DEFALUTPACKAGE.COM;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc1_test {
+	@Test
+	public void Tc1()
+	{
+Reporter.log("testcase1 is run");	
+
+}
+}
+
+
